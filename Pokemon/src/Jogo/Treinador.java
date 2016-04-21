c70e85094e603a25bcb34f779f74b0ac0b815eb4
garcinha <viniciusgarcias@hotmail.com>
2016-04-21 18:14:03 -0300

    Modificado 2

--- Pokemon/src/Jogo/Treinador.java
+++ Pokemon/src/Jogo/Treinador.java
@@ -1,7 +1,7 @@
 package Jogo;
 
 public class Treinador {
-	private Pokemon[] p = new Pokemon[10];
+	public Pokemon[] p = new Pokemon[10];
 	String nome;
 	
 	public Treinador ( String nome, Pokemon[] p, int npoke){
@@ -13,9 +13,9 @@ public class Treinador {
 	public static void main (String[] args){
 		Pokemon[] v = new Pokemon[10];
 		Ataque splash = new Ataque("splash", 10, 2);
-		v[0] = Pokemon("bla", 12, "agua", splash, splash, splash, splash);
-		v[1] = Pokemon("bla3", 12, "agua", splash, splash, splash, splash);
-		v[2] = Pokemon("bla2", 12, "agua", splash, splash, splash, splash);
+		v[0] = new Pokemon("bla", 12, "agua", splash, splash, splash, splash);
+		v[1] = new Pokemon("bla3", 12, "agua", splash, splash, splash, splash);
+		v[2] = new Pokemon("bla2", 12, "agua", splash, splash, splash, splash);
 		Treinador garca = new Treinador("garcia", v, 3);
 	}
 }
