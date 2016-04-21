6a2dc7f59633eefa08c48cf1c9ac55ad70d13fb3
garcinha <viniciusgarcias@hotmail.com>
2016-04-21 18:02:31 -0300

    Item

---
+++ Pokemon/src/Jogo/Item.java
@@ -0,0 +1,21 @@
+package Jogo;
+
+public class Item extends Evento{
+	private int tipo;
+	// 1 = pokeball
+	// 2 = potion
+	
+	static public void executa ( Treinador a, Treinador b, tipo ){
+		if ( tipo == 1 ){
+		}
+
+		if ( tipo == 2 ){
+		a.p[0].hp += 20;  
+			if ( a.p[0].hp > a.p[0].hpMax )
+				a.p[0].hp = a.p[0].hp.Max;
+		
+		}
+	
+		
+	}
+}
--- Pokemon/src/Jogo/Pokemon.java
+++ Pokemon/src/Jogo/Pokemon.java
@@ -3,6 +3,7 @@ package Jogo;
 public class Pokemon {
 	String nome;
 	int hp;
+	int hpMax;
 	String tipo;
 	Ataque a1;
 	Ataque a2;
@@ -12,6 +13,7 @@ public class Pokemon {
 	public Pokemon ( String nome, int hp, String tipo, Ataque a1, Ataque a2, Ataque a3, Ataque a4 ){
 		this.nome = nome;
 		this.hp = hp;
+		hpMax = hp;
 		this.tipo = tipo;
 		this.a1 = a1;
 		this.a2 = a2;
