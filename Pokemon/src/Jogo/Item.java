e9d469660e5939605483639f402614b1df81bcdf
mac0321 04 <mac0321.04@metralha.cec.ime.usp.br>
2016-04-28 10:35:33 -0300

    Commitei class ao inves de java

--- Pokemon/src/Jogo/Item.java
+++ Pokemon/src/Jogo/Item.java
@@ -1,10 +1,12 @@
 package Jogo;
+import java.util.Random;
 
 public class Item extends Evento{
-	// 1 = pokeball
-	// 2 = potion
-	int tipo;
-	
+	private static Random rand;
+	private static int prob = 0;
+	private int tipo;
+	// Item do tipo 1 = pokeball
+	// Item do tipo 2 = potion
 	
 	public Item (Treinador a, Treinador b, int tipo){
 		ator = a;
@@ -13,17 +15,27 @@ public class Item extends Evento{
 		prioridade = 1;
 	}	
 	
+	public static boolean capturou ( Treinador a ){
+		prob = 100 - ( (a.p[0].hp) / (a.p[0].hpMax) );
+		int randomNum = rand.nextInt(100);
+		if ( randomNum < prob )	return true;
+		else return false;
+	}
+	
 	public void executar (){
+		// 1 = POKEBALL
 		if ( tipo == 1 ){
 			
 		}
+		
+		// 2 = POTION
 		if ( tipo == 2 ){
 			if ( ator.p[0].hp+20 > ator.p[0].hpMax ){
-				System.out.println("O pokémon "+ator.p[0].nome+" recuperou "+(ator.p[0].hpMax - ator.p[0].hp)+" pontos de vida"  );
+				System.out.println("O pokï¿½mon "+ator.p[0].nome+" recuperou "+(ator.p[0].hpMax - ator.p[0].hp)+" pontos de vida"  );
 				ator.p[0].hp = ator.p[0].hpMax;	
 			}
 			else {
-				System.out.println("O pokémon "+ator.p[0].nome+" recuperou 20 pontos de vida" );
+				System.out.println("O pokï¿½mon "+ator.p[0].nome+" recuperou 20 pontos de vida" );
 				ator.p[0].hp += 20;
 			}
 		}
